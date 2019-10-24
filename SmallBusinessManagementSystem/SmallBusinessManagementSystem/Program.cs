ba1d9631882bfe430640117e588117a6223e3b2b
Md.Shafiqur Rahman <41997621+shafiqshuvo@users.noreply.github.com>
2019-10-24 22:41:37 +0600

    Update Program.cs

--- SmallBusinessManagementSystem/SmallBusinessManagementSystem/Program.cs
+++ SmallBusinessManagementSystem/SmallBusinessManagementSystem/Program.cs
@@ -17,8 +17,8 @@ namespace SmallBusinessManagementSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new SalesUI());
-           // Application.Run(new PurchaseUI());
+            Application.Run(new SalesUI());
+            Application.Run(new PurchaseUI());
             Application.Run(new SupplierEntryUI());
         }
     }
